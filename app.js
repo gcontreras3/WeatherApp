a5d270db38f582f6630874cf305aaac27f57d822
Ivan Contreras <gcontreras31996@gmail.com>
2023-06-06 09:20:59 -0500

    setup backend call to get url to Page.html

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Weather App
+// These are the packages that will be used in the application
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const https = require("https")
+
+// Here created our route for URL to Page.html
+app.use(bodyParser.urlencoded({ extended:true }))
+app.get("/", function (req, res){
+    res.sendFile(__direname + "/page.html");
+});
