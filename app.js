897e075c83a9f6cbe98ea8bbb64fd110876efb6f
Ivan Contreras <gcontreras31996@gmail.com>
2023-06-06 09:55:58 -0500

    finished setting up API CALL

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ const app = express()
 const bodyParser = require("body-parser")
 const https = require("https")
 
-// Here created our route for URL to Page.html
+// Created our route for URL to Page.html
 app.use(bodyParser.urlencoded({ extended:true }))
 app.get("/", function (req, res){
     res.sendFile(__direname + "/page.html");
 });
+
+// Here we will implement our API CALL to our URL
+
+app.post("/", function(res, req){
+    const cityName = req.body.cityName
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=30.2672&lon=97.7431&appid=064b6b9e0084795ff796c8bd93aba1de&units=imperial`
+    https.get(url, function (response){
+        response.on("data", function(data){
+            const jsondata = JSON.parse(data)
+            const temp = jsondata.main.temp
+            const desc = jsondata.weather[0].description
+            const icon = jsondata.weather[0].icon
+            // This imageURL will add an icon depending on the weather
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write(`<h1>The temperature in ${cityName} is ${temp} degrees. </h1>`)
+            res.write(`<p>The weather description is ${desc}</p>`)
+            res.write(`<img src=${imageURL}>`)
+        })
+    })
+})
+app.listen(9003)
